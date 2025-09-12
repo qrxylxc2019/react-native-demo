52230419beba1729a5385a7aba89534402cac501
lxc619 <lxc619@noreply.gitcode.com>
2025-09-12 17:29:55 +0800

    优化

---
+++ fwz/react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  dependencies: {
+    'react-native-vector-icons': {
+      platforms: {
+        android: null, // 完全禁用Android平台的自动链接
+      },
+    },
+  },
+  assets: ['./assets/fonts/'],
+};
